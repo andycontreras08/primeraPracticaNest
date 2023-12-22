83cc54f195fb8e5d813b59e99173b99d5255b0ec
andycontreras08 <dsm20190452.jhernandez@alumnos.utsv.edu.mx>
2023-12-22 10:34:32 -0600

    Commit del proyecto reorganizado completo

--- apps/tasks-status/app.module.ts
+++ apps/tasks-status/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ConexionModule } from '../../libs/modules/database/conexion/conexion.module';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
     imports: [
-        MongooseModule.forRoot(
-            'mongodb+srv://jhernandez:hayhainformatic08@andresmda.nuip01q.mongodb.net/tasks?retryWrites=true&w=majority'
-        ),
-        TodoModule,
+        ConexionModule,
+        TodoModule
     ]
 })
 export class AppModule {}
